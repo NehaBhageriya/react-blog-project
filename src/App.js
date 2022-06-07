244660fd6ec78dec0eb256b9d3c9ece548431048
NehaBhageriya <81167730+NehaBhageriya@users.noreply.github.com>
2022-06-07 18:34:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./Component/Header";
 import NavBar from "./Component/NavBar";
 import Home from "./Pages/Home"
